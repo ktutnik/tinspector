42c463636f2e29e1dc2a870933f579697db7c31c
Ketut Sandiarsa <ktutnik@gmail.com>
2020-02-23 16:52:26 +0800

    chore: Upgrade dependencies

--- jest.config.debug.js
+++ jest.config.debug.js
@@ -1,5 +1,4 @@
 module.exports = {
-    "testEnvironment": "node",
     "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.js?$",
     "snapshotSerializers": [
       "./scripts/function-snapshot-serializer.js"
