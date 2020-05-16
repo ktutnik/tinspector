5d328b1ec5d44b5f27e559aa7049a6f3fc1310d1
Ketut Sandiarsa <ktutnik@gmail.com>
2020-05-16 14:53:07 +0800

    chore: Upgrade dependencies (#54)

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,6 @@
-import * as src from "../src"
 
 describe("Exported", () => {
     it("Should export members", () => {
-        expect(Object.keys(src)).toMatchSnapshot()
+        expect(require("../src")).toMatchSnapshot()
     })
 })
