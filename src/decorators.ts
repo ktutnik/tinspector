94949e2412ec93b7cb1ddce25b7d404ec7f436ae
Ketut Sandiarsa <ktutnik@gmail.com>
2020-11-15 14:06:07 +0800

    Revert "fix: Fix data type inheritance issue (#90)"
    This reverts commit 01f7e2223ca0ced7991b52e8d5140ad64eb74cb9.

--- src/decorators.ts
+++ src/decorators.ts
@@ -17,6 +17,7 @@ import {
     PrivateDecorator,
     TypeDecorator,
     TypeOverride,
+    TypeOverrideOption,
 } from "./types"
 
 // --------------------------------------------------------------------- //
